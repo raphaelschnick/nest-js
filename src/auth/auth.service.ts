2be1a9c35dd2cf9f15a72c65147c5c9aa75c2b89
raphaelschnick <raphael.schnick@sybit.de>
2022-05-25 17:44:58 +0200

    lint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,15 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (user && user.password === pass) {
+      const { ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
   async login(user: any) {
     const payload = { username: user.username, sub: user.userId };
     return {
